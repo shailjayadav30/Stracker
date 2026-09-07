0ba56e184b9bba51f8cf1c5c4cedfec39fc5ae60
shailjayadav30 <shailjayadav7275@gmail.com>
2026-09-07 12:19:05 +0530

    fix: enhance PDF parsing by including CanvasFactory in PDFParse initialization

--- src/lib/parsePdf.ts
+++ src/lib/parsePdf.ts
@@ -1,6 +1,8 @@
+import "pdf-parse/worker";
+import { CanvasFactory } from "pdf-parse/worker";
 import { PDFParse } from "pdf-parse";
 export async function parsePdf(pdf: Buffer) {
-  const pdfData = new PDFParse({ data: pdf });
+  const pdfData = new PDFParse({ data: pdf,CanvasFactory });
   const extractedText = await pdfData.getText();
   if (!extractedText) {
     throw new Error("Could not extract any text from this PDF");
