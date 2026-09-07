ab08ec247f7f941c6733efb991b5cb35b3de1811
shailjayadav30 <shailjayadav7275@gmail.com>
2026-09-07 11:11:04 +0530

    fix: refactor express type definitions to use module augmentation for Request interface

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,10 +1,20 @@
+// import { auth } from "../lib/auth.js";
+// declare global {
+//   namespace Express {
+//     interface Request {
+//       user?: typeof auth.$Infer.Session.user;
+//     }
+//   }
+// }
+
+// export {};
+
+
+// src/types/express.d.ts
 import { auth } from "../lib/auth.js";
-declare global {
-  namespace Express {
-    interface Request {
-      user?: typeof auth.$Infer.Session.user;
-    }
+
+declare module "express-serve-static-core" {
+  interface Request {
+    user?: typeof auth.$Infer.Session.user;
   }
 }
-
-export {};
